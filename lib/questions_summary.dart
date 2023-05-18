01214965795d6bb82d1ef329c1d2354b3f39332d
c-abbott <cllmbbtt@gmail.com>
2023-05-18 16:37:01 +0000

    Summary formatting complete

--- lib/questions_summary.dart
+++ lib/questions_summary.dart
@@ -9,19 +9,33 @@ class QuestionsSummary extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 400,
+      height: 500,
       child: SingleChildScrollView(
         child: Column(
           children: summaryData.map(
             (data) {
+              bool isCorrect = (data['user_answer'] == data['correct_answer']);
               return Row(
                 children: [
-                  Text(
-                    ((data['question_index'] as int) + 1).toString(),
-                    style: const TextStyle(color: skWhite),
+                  Container(
+                    margin: const EdgeInsets.all(16),
+                    padding: const EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                      color: isCorrect ? correctAnswerColor : wrongAnswerColor,
+                      shape: BoxShape.circle,
+                    ),
+                    child: Text(
+                      ((data['question_index'] as int) + 1).toString(),
+                      style: const TextStyle(
+                          color: skWhite,
+                          fontSize: 20,
+                          fontFamily: 'ProximaNova',
+                          fontWeight: FontWeight.bold),
+                    ),
                   ),
                   Expanded(
                     child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Text(
                           data['question'] as String,
@@ -33,26 +47,59 @@ class QuestionsSummary extends StatelessWidget {
                               fontWeight: FontWeight.bold),
                         ),
                         const SizedBox(height: 4),
-                        Text(
-                          data['user_answer'] as String,
-                          style: TextStyle(
-                            color:
-                                (data['user_answer'] == data['correct_answer'])
-                                    ? correctAnswerColor
-                                    : wrongAnswerColor,
-                            fontFamily: 'ProximaNova',
-                            fontWeight: FontWeight.w400,
-                            fontSize: 10,
+                        RichText(
+                          text: TextSpan(
+                            children: [
+                              TextSpan(
+                                text: 'Your answer: ',
+                                style: TextStyle(
+                                  color: isCorrect
+                                      ? correctAnswerColor
+                                      : wrongAnswerColor,
+                                  fontFamily: 'ProximaNova',
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 12,
+                                ),
+                              ),
+                              TextSpan(
+                                text: '${data['user_answer']}',
+                                style: TextStyle(
+                                  color: isCorrect
+                                      ? correctAnswerColor
+                                      : wrongAnswerColor,
+                                  fontFamily: 'ProximaNova',
+                                  fontWeight: FontWeight.w400,
+                                  fontSize: 12,
+                                ),
+                              ),
+                            ],
                           ),
                         ),
                         const SizedBox(height: 4),
-                        Text(data['correct_answer'] as String,
-                            style: const TextStyle(
-                              color: correctAnswerColor,
-                              fontFamily: 'ProximaNova',
-                              fontWeight: FontWeight.w400,
-                              fontSize: 10,
-                            )),
+                        RichText(
+                          text: TextSpan(
+                            children: [
+                              const TextSpan(
+                                text: 'Correct answer: ',
+                                style: TextStyle(
+                                  color: correctAnswerColor,
+                                  fontFamily: 'ProximaNova',
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 12,
+                                ),
+                              ),
+                              TextSpan(
+                                text: '${data['correct_answer']}',
+                                style: const TextStyle(
+                                  color: correctAnswerColor,
+                                  fontFamily: 'ProximaNova',
+                                  fontWeight: FontWeight.w400,
+                                  fontSize: 12,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
                       ],
                     ),
                   ),
